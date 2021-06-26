d94b0d45524406a2443c06e64378cb8beb925fa8
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-06-25 22:23:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CShp_StringReverse
+{
+        class URI
+    {
+        public static string InverteString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Informe a sequência de números: ");                  
+            string n = Console.ReadLine();
+
+            string v = new string(n.Reverse().ToArray());
+            Console.WriteLine(); 
+            Console.WriteLine($"{v}");
+        }
+    }
+}
